2693136c2af12534011cd96adbe310aa5e3ff1a1
Karlos <hovhannisyan1030@gmail.com>
2022-10-15 04:23:19 +0400

    frond end

---
+++ src/test/java/com/example/fruitProject/FruitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fruitProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
